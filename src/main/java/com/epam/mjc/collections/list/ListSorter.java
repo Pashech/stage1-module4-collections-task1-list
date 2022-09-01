91b711e4d3c3f5d1f60e6b60c5df4e41196201bf
Pavel Setsko <Pashech555@gmail.com>
2022-09-01 18:39:52 +0300

    done

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,26 @@
 package com.epam.mjc.collections.list;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int result1 = function(Integer.parseInt(a));
+        int result2 = function(Integer.parseInt(b));
+
+        if (result1 == result2) {
+            return Integer.parseInt(a) - Integer.parseInt(b);
+        }
+        return result1 - result2;
+    }
+
+    private int function(int a) {
+        return 5 * a * a + 3;
     }
 }
